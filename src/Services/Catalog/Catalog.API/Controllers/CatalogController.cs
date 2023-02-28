fb0fa96d56cb33a32d2a426bc618de6df566e311
aitor ambite <aitorambite@protonmail.com>
2023-02-28 23:20:46 +0100

    Añadido repository,seed, controller etc

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger _logger;
+
+        public CatalogController(IProductRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> getProducts() {
+            IEnumerable<Product> products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("id:lenght(24)",Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> getProduct(string id)
+        {
+            Product product = await _repository.GetProduct(id);
+            if (product == null) {
+                _logger.LogError($"Product with id: {id},not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration config)
+        {
+            string connectionString = config.GetValue<String>("DatabaseSettings:ConnectionString");
+            string databaseName = config.GetValue<String>("DatabaseSettings:databaseName");
+            string CollectionString = config.GetValue<String>("DatabaseSettings:CollectionName");
+
+            MongoClient Client = new MongoClient(connectionString);
+            IMongoDatabase database = Client.GetDatabase(databaseName);
+            Products = database.GetCollection<Product>(CollectionString); 
+            CatalogContextSeed.seedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; set; }
+    }
+}
